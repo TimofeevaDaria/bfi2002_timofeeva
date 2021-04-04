2aedf0728efba6181b2cd232570e113568acd680
TimofeevaDaria <80330784+TimofeevaDaria@users.noreply.github.com>
2021-04-04 22:17:37 +0300

    Create Primes.java

---
+++ java_Tasks/Task1/Primes.java
@@ -0,0 +1,15 @@
+public class Primes {
+    public static void main (String[] args){
+        for (int i = 2; i<=100; i++) {
+        if (iSPrime(i))
+            System.out.print(i + "  ");
+        }
+    }
+    public static boolean iSPrime (int n){
+        for (int i = 2; i<n; i++){
+            if (n%i==0)
+                    return false;
+        }
+        return true;
+    }
+}
